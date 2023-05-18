1b92bbc88d3cc89dede2c835bba979a311c5d880
Kelvin <Kelzjr56@gmail.com>
2023-05-18 15:33:28 -0400

    Added upload feature for external images on user's computer.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from PIL import Image
 
+uploaded_image = st.file_uploader("Upload Image")
+
+
 with st.expander("Start Camera"):
     # Enable the camera
     camera_image = st.camera_input("Camera Image")
@@ -18,4 +21,8 @@ if camera_image:
     st.image(gray_img)
 
 
-
+# Converter for uploading image from an external file
+if uploaded_image:
+    up_img = Image.open(uploaded_image)
+    gray_img_upload = up_img.convert("L")
+    st.image(gray_img_upload)
